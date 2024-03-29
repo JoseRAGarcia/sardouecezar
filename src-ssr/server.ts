b9ed9c32a6478c66b2e940789bf2aee469a5d233
joseragarcia <jose.ragarcia82@gmail.com>
2024-03-29 18:25:40 -0300

    feat: Criada configuração de vercel

--- src-ssr/server.ts
+++ src-ssr/server.ts
@@ -14,7 +14,7 @@ import compression from 'compression';
 import {
   ssrClose,
   ssrCreate,
-  ssrListen,
+  // ssrListen,
   ssrRenderPreloadTag,
   ssrServeStaticContent,
 } from 'quasar/wrappers';
@@ -53,14 +53,25 @@ export const create = ssrCreate((/* { ... } */) => {
  * For production, you can instead export your
  * handler for serverless use or whatever else fits your needs.
  */
-export const listen = ssrListen(async ({ app, port, isReady }) => {
-  await isReady();
-  return app.listen(port, () => {
-    if (process.env.PROD) {
-      console.log('Server listening at port ' + port);
-    }
-  });
-});
+export async function listen({ app, port, isReady, ssrHandler }: any) {
+  if (process.env.DEV) {
+    await isReady();
+    return await app.listen(port, () => {
+      if (process.env.PROD) {
+        console.log('Server listening at port ' + port);
+      }
+    });
+  } else {
+    // in production
+    // "ssrHandler" is a prebuilt handler which already
+    // waits for all the middlewares to run before serving clients
+
+    // whatever you return here is equivalent to module.exports.<key> = <value>
+    return { handler: ssrHandler };
+  }
+}
+
+
 
 /**
  * Should close the server and free up any resources.
