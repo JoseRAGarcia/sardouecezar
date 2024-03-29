e802d9e3a89e6e45560a5654c082186bdc59924d
joseragarcia <jose.ragarcia82@gmail.com>
2024-03-29 18:07:22 -0300

    feat: Criado header e homepage

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,15 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header class="bg-primary-50">
-      <q-toolbar class="ovreflow-hidden" style="height: 110px">
+    <q-header class="bg-primary-50" style="height: 110px">
+      <q-toolbar
+        v-if="!$q.screen.xs && !$q.screen.sm"
+        class="overflow-hidden full-height"
+      >
         <div class="container">
           <div class="full-width full-height flex justify-between items-center">
             <router-link class="logo-container q-pa-sm" :to="{ name: 'home' }">
               <img
-                src="/img/logo_wbg.png"
+                src="/img/logo/logo_wbg.png"
                 class="logo"
                 alt="logomarca"
                 loading="lazy"
@@ -72,7 +75,7 @@
                   class="q-ml-md"
                   rounded
                   no-wrap
-                  color="accent"
+                  color="positive"
                   label="Faça uma cotação"
                   style="height: 51px"
                 />
@@ -81,11 +84,81 @@
           </div>
         </div>
       </q-toolbar>
+      <q-toolbar v-else class="overflow-hidden full-height">
+        <div class="full-width full-height flex justify-between items-center">
+          <router-link class="logo-container q-pa-sm" :to="{ name: 'home' }">
+            <img
+              src="/img/logo/logo_wbg.png"
+              class="logo"
+              alt="logomarca"
+              loading="lazy"
+            />
+          </router-link>
+          <q-btn flat color="white" icon="menu">
+            <q-menu class="bg-primary">
+              <q-list
+                class="text-white text-no-wrap"
+                style="font-size: 1rem; min-width: 200px"
+              >
+                <q-item
+                  clickable
+                  v-ripple
+                  :active="$route.name === 'home'"
+                  :to="{ name: 'home' }"
+                  active-class="text-accent text-weight-bold"
+                >
+                  <q-item-section>Início</q-item-section>
+                </q-item>
+                <q-item
+                  clickable
+                  v-ripple
+                  :active="$route.name === 'about'"
+                  :to="{ name: 'about' }"
+                  active-class="text-accent text-weight-bold"
+                >
+                  <q-item-section>Sobre nós</q-item-section>
+                </q-item>
+                <q-item
+                  clickable
+                  v-ripple
+                  :active="$route.name === 'produtos'"
+                  :to="{ name: 'produtos' }"
+                  active-class="text-accent text-weight-bold"
+                >
+                  <q-item-section>Produtos</q-item-section>
+                </q-item>
+                <q-item
+                  clickable
+                  v-ripple
+                  :active="$route.name === 'sinistro'"
+                  :to="{ name: 'sinistro' }"
+                  active-class="text-accent text-weight-bold"
+                >
+                  <q-item-section>Sinistro</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+        </div>
+      </q-toolbar>
     </q-header>
     <q-page-container>
       <router-view />
     </q-page-container>
-    <q-page-sticky position="bottom-right" :offset="[18, 18]">
+    <q-page-sticky
+      v-if="$q.screen.xs || $q.screen.sm"
+      position="bottom-left"
+      class="full-width bg-positive cursor-pointer relative-position"
+      v-ripple
+    >
+      <div
+        class="text-white flex flex-center text-no-wrap text-uppercase"
+        style="height: 51px; font-size: 1rem"
+      >
+        Faça uma cotação
+      </div>
+    </q-page-sticky>
+    <q-page-sticky position="bottom-right" :offset="[18, 70]">
       <q-btn round color="positive" icon="bi-whatsapp" size="16px" />
     </q-page-sticky>
   </q-layout>
@@ -102,7 +175,7 @@ export default defineComponent({
 <style scoped>
 .logo-container {
   height: 110px;
-  width: 200px;
+  width: 110px;
   display: flex;
   align-items: center;
   justify-content: center;
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-page class="row items-start justify-evenly">
-    <div class="home-banner shadow-2">
+  <q-page class="column items-center justify-start q-gutter-y-lg bg-white">
+    <div class="home-banner">
       <q-carousel
         animated
         v-model="slide"
@@ -27,6 +27,158 @@
           img-src="https://cdn.quasar.dev/img/quasar.jpg"
         />
       </q-carousel>
+    </div>
+    <div class="container q-px-lg q-gutter-y-lg">
+      <div class="flex items-start relative-position">
+        <div
+          :style="
+            !$q.screen.xs && !$q.screen.sm
+              ? 'width: calc(100% - 400px); padding-right: 15px'
+              : 'width: 100%'
+          "
+        >
+          <h2 class="text-h4 text-weight-bold text-dark">Quem somos?</h2>
+          <p class="text-body1">
+            Somos uma empresa familiar e nossa sede fica em Duque de Caxias, Rio
+            de Janeiro. Atuamos no mercado de seguros há quase duas décadas.
+            Realizamos nossas cotações em mais de 15 seguradoras, sempre
+            buscando o melhor custo-benefício para nossos segurados. Quer
+            realizar um orçamento ou tem alguma dúvida sobre nossa empresa?
+            Clique no link da bio, estamos à disposição! Será um prazer te
+            atender!
+          </p>
+          <div
+            v-if="$q.screen.xs || $q.screen.sm"
+            class="absolute"
+            style="bottom: 0; right: 0; opacity: 0.1"
+          >
+            <transition
+              appear
+              enter-active-class="animated slideInRight"
+              leave-active-class="animated slideInLeft"
+            >
+              <q-img src="/img/home/quem-somos2.png" width="200px" />
+            </transition>
+          </div>
+        </div>
+
+        <div v-if="!$q.screen.xs && !$q.screen.sm">
+          <transition
+            appear
+            enter-active-class="animated slideInRight"
+            leave-active-class="animated slideInLeft"
+          >
+            <q-img
+              src="/img/home/quem-somos2.png"
+              width="400px"
+              class="rounded-borders bg-light"
+            />
+          </transition>
+        </div>
+      </div>
+      <!--  -->
+
+      <div class="flex items-start relative-position">
+        <div v-if="!$q.screen.xs && !$q.screen.sm">
+          <transition
+            appear
+            enter-active-class="animated slideInLeft"
+            leave-active-class="animated slideInRight"
+          >
+            <q-img
+              src="/img/home/seguro-carro.png"
+              width="400px"
+              class="rounded-borders bg-light"
+            />
+          </transition>
+        </div>
+
+        <div
+          :style="
+            !$q.screen.xs && !$q.screen.sm
+              ? 'width: calc(100% - 400px); text-align: right; padding-left: 15px'
+              : 'width: 100%'
+          "
+        >
+          <h2 class="text-h4 text-weight-bold text-dark">
+            Proteja o seu carro
+          </h2>
+          <p class="text-body1">
+            Proteja seu veículo com tranquilidade. Com nossos produtos de seguro
+            para carros e motos, você pode dirigir com a certeza de estar
+            protegido em caso de imprevistos. Oferecemos uma ampla gama de
+            serviços e vantagens para garantir sua segurança e comodidade ao
+            volante. Dos carros mais robustos às ágeis motocicletas, nossos
+            seguros proporcionam benefícios exclusivos para todos os tipos de
+            veículos. Não deixe a preocupação atrapalhar sua jornada. Proteja
+            seu patrimônio com nossos produtos.
+          </p>
+          <div
+            v-if="$q.screen.xs || $q.screen.sm"
+            class="absolute"
+            style="bottom: 0; left: 0; opacity: 0.1"
+          >
+            <transition
+              appear
+              enter-active-class="animated slideInLeft"
+              leave-active-class="animated slideInRight"
+            >
+              <q-img src="/img/home/seguro-carro.png" width="200px" />
+            </transition>
+          </div>
+        </div>
+      </div>
+
+      <!--  -->
+
+      <div class="flex items-start relative-position">
+        <div
+          :style="
+            !$q.screen.xs && !$q.screen.sm
+              ? 'width: calc(100% - 400px); padding-right: 15px'
+              : 'width: 100%'
+          "
+        >
+          <h2 class="text-h4 text-weight-bold text-dark">Planos de saúde</h2>
+          <p class="text-body1">
+            Priorize sua saúde e bem-estar com nossos planos de saúde
+            abrangentes. Sabemos que cuidar da sua saúde é essencial para uma
+            vida plena. Com nossos planos, você e sua família têm acesso a uma
+            rede ampla de profissionais e serviços médicos de qualidade. Desde
+            consultas de rotina até procedimentos mais complexos, estamos aqui
+            para garantir o melhor atendimento em todos os momentos. Conte
+            conosco para cuidar da sua saúde e proporcionar tranquilidade para
+            você e seus entes queridos.
+          </p>
+          <div
+            v-if="$q.screen.xs || $q.screen.sm"
+            class="absolute"
+            style="bottom: 0; right: 0; opacity: 0.1"
+          >
+            <transition
+              appear
+              enter-active-class="animated slideInRight"
+              leave-active-class="animated slideInLeft"
+            >
+              <q-img src="/img/home/plano-saude.png" width="200px" />
+            </transition>
+          </div>
+        </div>
+
+        <div v-if="!$q.screen.xs && !$q.screen.sm">
+          <transition
+            appear
+            enter-active-class="animated slideInRight"
+            leave-active-class="animated slideInLeft"
+          >
+            <q-img
+              src="/img/home/plano-saude.png"
+              width="400px"
+              class="rounded-borders bg-light"
+            />
+          </transition>
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
